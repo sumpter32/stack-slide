608795a1f2084ce8158ee122dad4ec8ffe174349
steven sumpter <sumpter32@gmail.com>
2026-02-15 18:58:25 +0800

    Add Capacitor for Android build + GitHub Actions workflow

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'com.srs.stackslide',
+  appName: 'Stack Slide',
+  webDir: 'out'
+};
+
+export default config;
--- next.config.js
+++ next.config.js
@@ -1,3 +1,9 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
+const nextConfig = {
+  output: 'export',
+  images: {
+    unoptimized: true
+  },
+  trailingSlash: true
+}
 module.exports = nextConfig
